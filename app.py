18bc37a65566f369c4dd5993ed189e6c20f9e23e
iresc <127307618+iresc@users.noreply.github.com>
2025-03-12 11:24:18 +0100

    try1
    attempt to use a more complex code

--- app.py
+++ app.py
@@ -1,3 +1,26 @@
+# app.py, run with 'streamlit run app.py'
+import pandas as pd
 import streamlit as st
+#from streamlit_gsheets import GSheetsConnection
 
-st.write("Hello world")
+# Create a connection object.
+#conn = st.connection("gsheets", type=GSheetsConnection)
+
+#df = conn.read()
+
+# Print results.
+#for row in df.itertuples():
+#    st.write(f"Giorno: {row.Data}")
+
+df = pd.read_csv("laptops.csv")  # read a CSV file inside
+# df = pd.read_excel(...)  # will work for Excel files
+
+st.title("Visualizzazione dati laptop")  # add a title
+st.subheader("Anteprima dei dati")
+st.dataframe(df)
+
+st.subheader("Statistiche descrittive")
+st.write(df.describe())
+
+st.subheader("GPU - Prezzo")
+st.scatter_chart(df, x="GPU", y="Final Price")
