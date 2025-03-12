0bee6d1ba4481c20cf80091602d3fa86e652c7e8
db293 <dario.belvedere@hotmail.it>
2025-03-12 17:35:16 +0100

    Update dashboard.py
    ram + storage

--- dashboard.py
+++ dashboard.py
@@ -7,8 +7,18 @@ st.dataframe(df)
 
 brands = df["Brand"].unique().tolist() ## estrai valori unici di brand dal dataframe
 
-statuses=df["Status"].unique().tolist() ## estrai valori unici di stato
+statuses = df["Status"].unique().tolist() ## estrai valori unici di stato
+
+rams = df["Ram"].unique().tolist() ## estrai valori unici di ram
+
+storages = df["Storage"].unique().tolist() ## estrai valori unici di storage
+
 
 selectbox_marca = st.sidebar.selectbox('Scegli una marca',brands)
 
 selectbox_stato = st.sidebar.selectbox('In che stato vuoi che sia?',statuses)
+
+selectbox_ram = st.sidebar.selectbox('Quanta RAM minima?',rams)
+
+selectbox_storages = st.sidebar.selectbox('Quanta ROM minima?',storages)
+
