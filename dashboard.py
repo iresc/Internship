fd64e6d063380f40d975733555ab05901de7502c
db293 <dario.belvedere@hotmail.it>
2025-03-12 17:32:06 +0100

    Update dashboard.py
    stato

--- dashboard.py
+++ dashboard.py
@@ -7,4 +7,8 @@ st.dataframe(df)
 
 brands = df["Brand"].unique().tolist() ## estrai valori unici di brand dal dataframe
 
+statuses=df["Status"].unique().tolist() ## estrai valori unici di stato
+
 selectbox_marca = st.sidebar.selectbox('Scegli una marca',brands)
+
+selectbox_stato = st.sidebar.selectbox('In che stato vuoi che sia?',brands)
