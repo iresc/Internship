5d54c10a216b7572af39d01d00b5bc22f050f1b3
db293 <dario.belvedere@hotmail.it>
2025-03-12 17:29:22 +0100

    Update dashboard.py
    added dataframe

--- dashboard.py
+++ dashboard.py
@@ -4,3 +4,7 @@ import pandas as pd
 df = pd.read_csv("laptops.csv") ## dataframe con dati laptop
 
 st.dataframe(df)
+
+brands = df["brand"].unique().tolist() ## estrai valori unici di brand dal dataframe
+
+selectbox_marca = st.sidebar.selectbox('Scegli una marca',brands)
