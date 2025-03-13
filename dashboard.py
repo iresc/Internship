5575d04de83c1f8685228bc99354c615400c0e26
db293 <dario.belvedere@hotmail.it>
2025-03-13 12:07:56 +0100

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -58,9 +58,8 @@ st.markdown('---')                                             # barra orizzonta
 
 
 
-st.title("ChatGPT-like clone")
 
-client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
+#client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
 
 if "openai_model" not in st.session_state:
     st.session_state["openai_model"] = "gpt-3.5-turbo"
