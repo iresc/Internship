f1745528b97e55a12b688accb4e9afe90d41ec36
db293 <dario.belvedere@hotmail.it>
2025-03-13 10:39:44 +0100

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -4,7 +4,7 @@ import pathlib
 
 def load_css(file_path):
     with open(file_path) as f:
-        st.html(f"<style>{f.read}</style>")
+        st.html("<style>{f.read}</style>")
 
 css_path = pathlib.Path("Styles/style.css")
 load_css(css_path)
