9e0ff2ddefd718b86fd21cfe7efc03ec6587f4e2
iresc <127307618+iresc@users.noreply.github.com>
2025-04-16 13:09:30 +0200

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -3,7 +3,7 @@ import pandas as pd
 import random
 import time
 from random import randrange
-#from streamlit_gsheets import GSheetsConnection
+from streamlit_gsheets import GSheetsConnection
 
 st.markdown("""
     <style>
@@ -26,53 +26,54 @@ conn = st.connection("gsheets", type=GSheetsConnection)
 # Carica il DataFrame iniziale solo una volta
 if "dff" not in st.session_state:
 #    st.session_state.dff = pd.read_csv("laptops.csv")
-     st.session_state.dff = conn.read
+     st.session_state.dff = conn.read()
 
 #df = pd.read_csv("laptops.csv")  # Carica il DataFrame
 df = conn.read()
 
+
 # Estrai valori unici dalle colonne
-brands = df["Brand"].unique().tolist()
-statuses = df["Status"].unique().tolist()
-rams = sorted(df["RAM"].unique().tolist())
-storages = sorted(df["Storage"].unique().tolist())
+#brands = df["Brand"].unique().tolist()
+#statuses = df["Status"].unique().tolist()
+#rams = sorted(df["RAM"].unique().tolist())
+#storages = sorted(df["Storage"].unique().tolist())
 
 # Definisci le fasce di prezzo predefinite
-price_ranges = {
-    "0 - 100": (0, 100),
-    "100 - 300": (100, 300),
-    "300 - 500": (300, 500),
-    "500 - 1000": (500, 1000),
-    "1000 - 2000": (1000, 2000),
-    "2000 - MAX": (2000, df["Final Price"].max())
-}
+#price_ranges = {
+#    "0 - 100": (0, 100),
+#    "100 - 300": (100, 300),
+#    "300 - 500": (300, 500),
+#    "500 - 1000": (500, 1000),
+#    "1000 - 2000": (1000, 2000),
+#    "2000 - MAX": (2000, df["Final Price"].max())
+#}
 
 # Sidebar per i filtri
-selectbox_marca = st.sidebar.selectbox('Scegli una marca', brands)
-selectbox_stato = st.sidebar.selectbox('In che stato vuoi che sia?', statuses)
-selectbox_ram = st.sidebar.selectbox('Quanta RAM minima?', rams)
-selectbox_storages = st.sidebar.selectbox('Quanta ROM minima?', storages)
+#selectbox_marca = st.sidebar.selectbox('Scegli una marca', brands)
+#selectbox_stato = st.sidebar.selectbox('In che stato vuoi che sia?', statuses)
+#selectbox_ram = st.sidebar.selectbox('Quanta RAM minima?', rams)
+#selectbox_storages = st.sidebar.selectbox('Quanta ROM minima?', storages)
 
 # Selectbox per la fascia di prezzo
-selected_range = st.sidebar.selectbox("Seleziona fascia di prezzo", list(price_ranges.keys()))
-min_price, max_price = price_ranges[selected_range]
+#selected_range = st.sidebar.selectbox("Seleziona fascia di prezzo", list(price_ranges.keys()))
+#min_price, max_price = price_ranges[selected_range]
 
 # Bottone per applicare il filtro
-if st.sidebar.button('Filtra'):
-    st.session_state.dff = df[
-        (df["Brand"] == selectbox_marca) &
-        (df["Status"] == selectbox_stato) &
-        (df["RAM"] >= selectbox_ram) &
-        (df["Storage"] >= selectbox_storages) &
-        (df["Final Price"].between(min_price, max_price))
-    ]
+#if st.sidebar.button('Filtra'):
+#    st.session_state.dff = df[
+#        (df["Brand"] == selectbox_marca) &
+#        (df["Status"] == selectbox_stato) &
+#        (df["RAM"] >= selectbox_ram) &
+#        (df["Storage"] >= selectbox_storages) &
+#        (df["Final Price"].between(min_price, max_price))
+#    ]
 
 # bottone per resettare i filtri
-if st.sidebar.button('Resetta i filtri'):
-    st.session_state.dff = df
+#if st.sidebar.button('Resetta i filtri'):
+#    st.session_state.dff = df
 
 
-st.header('✨Trova il laptop più adatto alle tue esigenze✨') # titolo
+st.header('Title placeholder') # titolo
 st.markdown('---')                                             # barra orizzontale
 # Mostra il DataFrame aggiornato
 st.dataframe(st.session_state.dff)
